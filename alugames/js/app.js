12e74aca12ae73c6036007f2bc327109ab4a0547
Iruam Candido <iruamcandido@gmail.com>
2026-01-24 09:04:14 -0300

    implementando função alterarStatus

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,5 +1,9 @@
 function alterarStatus(id)
 {
-let gameClicado = document.querySelector(`game-${id}`);
+let gameClicado = document.getElementById(`game-${id}`);
+let imagem = gameClicado.querySelector('.dashboard__items__item');
+let botao = gameClicado.querySelector('.dashboard__item__button');
+let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+alert(nomeJogo.textContent);
 }
 
