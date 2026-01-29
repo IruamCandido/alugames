f577f8fd3abd6b9b3e23776b36ebb18b6ff243ed
Iruam Candido <iruamcandido@gmail.com>
2026-01-28 23:36:23 -0300

    Formatação de Código.

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -3,35 +3,31 @@ function alterarStatus(id) {
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
 
-if (imagem.classList.contains('dashboard__item__img--rented')) {
-    imagem.classList.remove('dashboard__item__img--rented');
-    botao.classList.remove('dashboard__item__button--return');
-    botao.textContent = 'Alugar';
-    jogosalugados --
-    jogosAlugados();
-    
-//No projeto Alugames, uma confirmação ao devolver um jogo,
-// solicitando ao usuário que confirme a devolução antes que ela seja concluída.
-// Isso pode ajudar a evitar devoluções acidentais
-    let confirmação = prompt('Deseja devolver o jogo? SIM - digite 1; Não - digite qualquer outro número')
-    if(confirmação == 1)
-        {
-        alert('Devolução Concluída');
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+        jogosalugados--
+        jogosAlugados();
+        //No projeto Alugames, uma confirmação ao devolver um jogo,
+        // solicitando ao usuário que confirme a devolução antes que ela seja concluída.
+        // Isso pode ajudar a evitar devoluções acidentais
+        let confirmação = prompt('Deseja devolver o jogo? SIM - digite 1; Não - digite qualquer outro número')
+        if (confirmação == 1) {
+            alert('Devolução Concluída');
         }
-        else if(confirmação <0 || confirmação == 0 || confirmação > 1)
-        {
+        else if (confirmação < 0 || confirmação == 0 || confirmação > 1) {
+            imagem.classList.add('dashboard__item__img--rented');
+            botao.classList.add('dashboard__item__button--return');
+            botao.textContent = 'Devolver';
+        }
+    }
+    else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
-        }
-}
-else
-    {
-    imagem.classList.add('dashboard__item__img--rented');
-    botao.classList.add('dashboard__item__button--return');
-    botao.textContent = 'Devolver';
-    jogosalugados ++;
-    jogosAlugados();
+        jogosalugados++;
+        jogosAlugados();
     }
 }
 let jogosalugados = 1;
